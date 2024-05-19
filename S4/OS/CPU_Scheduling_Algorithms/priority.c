113d382fe622b31776a1aa5089caec69828fec43
Navaneeth K.B <navaneethkb04@gmail.com>
2024-05-20 02:04:58 +0530

    Update priority.c

--- S4/OS/CPU_Scheduling_Algorithms/priority.c
+++ S4/OS/CPU_Scheduling_Algorithms/priority.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 
-struct Process {
+struct Process 
+{
     int pid;
     int arrivalTime;
     int burstTime;
@@ -8,83 +9,75 @@ struct Process {
     int turn_aroundtime;
     int completionTime;
     int priority;
-};
+} p[10];
 
-struct Process processes[10];
+void main() 
+{
+    int n;
+    printf("Enter no of p:");
+    scanf("%d", &n);
+    // Input process details
+    for (int i = 0; i < n; i++) {
+        printf("%d:Enter pid:", i);
+        scanf("%d", &p[i].pid);
+        printf("\nEnter arrival time:");
+        scanf("%d", &p[i].arrivalTime);
+        printf("\nEnter burst time:");
+        scanf("%d", &p[i].burstTime);
+        printf("\nEnter priority:");
+        scanf("%d", &p[i].priority);
+    }
 
-void sort(int n) {
+    // Sort processes based on priority
     struct Process temp;
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - 1 - i; j++) {
-            if (processes[j].priority > processes[j + 1].priority) {
-                temp = processes[j];
-                processes[j] = processes[j + 1];
-                processes[j + 1] = temp;
+    for (int i = 0; i < n - 1; i++) 
+    {
+        for (int j = 0; j < n - 1 - i; j++) 
+        {
+            if (p[j].priority > p[j + 1].priority) 
+            {
+                temp = p[j];
+                p[j] = p[j + 1];
+                p[j + 1] = temp;
             }
         }
     }
-}
 
-void findWaitingTime(int n) {
-    for (int i = 0; i < n; i++) {
-        processes[i].waitingTime = processes[i].turn_aroundtime - processes[i].burstTime;
-    }
-}
-
-void findTurnAroundTime(int n) {
-    for (int i = 0; i < n; i++) {
-        processes[i].turn_aroundtime = processes[i].completionTime - processes[i].arrivalTime;
-    }
-}
-
-void findCompletionTime(int n) {
-    processes[0].completionTime = processes[0].arrivalTime + processes[0].burstTime;
-    for (int i = 1; i < n; i++) {
-        int currentTime = processes[i - 1].completionTime;
-        if (processes[i].arrivalTime < currentTime) {
-            processes[i].completionTime = processes[i - 1].completionTime + processes[i].burstTime;
-        } else {
-            processes[i].completionTime = processes[i].arrivalTime + processes[i].burstTime;
+    // Find completion time
+    p[0].completionTime = p[0].arrivalTime + p[0].burstTime;
+    for (int i = 1; i < n; i++) 
+    {
+        int currentTime = p[i - 1].completionTime;
+        if (p[i].arrivalTime < currentTime) 
+        {
+            p[i].completionTime = p[i - 1].completionTime + p[i].burstTime;
+        } 
+        else 
+        {
+            p[i].completionTime = p[i].arrivalTime + p[i].burstTime;
         }
     }
-}
 
-void findAvg(int n) {
+    // Find turnaround time and waiting time
     int total_wt = 0, total_tt = 0;
-    for (int i = 0; i < n; i++) {
-        total_wt += processes[i].waitingTime;
-        total_tt += processes[i].turn_aroundtime;
+    for (int i = 0; i < n; i++) 
+    {
+        p[i].turn_aroundtime = p[i].completionTime - p[i].arrivalTime;
+        p[i].waitingTime = p[i].turn_aroundtime - p[i].burstTime;
+        total_wt += p[i].waitingTime;
+        total_tt += p[i].turn_aroundtime;
     }
+
+    // Print process details
+    printf("PID\tAT\tBT\tCT\tTAT\tWT");
+    for (int i = 0; i < n; i++) 
+    {
+        printf("\n%d\t%d\t%d\t%d\t%d\t%d", p[i].pid, p[i].arrivalTime, p[i].burstTime, p[i].completionTime, p[i].turn_aroundtime, p[i].waitingTime);
+    }
+
+    // Print averages
     printf("\nAverage wait time = %f", (float)total_wt / (float)n);
     printf("\nAverage turn around time = %f", (float)total_tt / (float)n);
-}
 
-int main() {
-    int n;
-    printf("Enter no of processes:");
-    scanf("%d", &n);
-    for (int i = 0; i < n; i++) {
-        printf("%d:Enter pid:", i);
-        scanf("%d", &processes[i].pid);
-        printf("\nEnter arrival time:");
-        scanf("%d", &processes[i].arrivalTime);
-        printf("\nEnter burst time:");
-        scanf("%d", &processes[i].burstTime);
-        printf("\nEnter priority:");
-        scanf("%d", &processes[i].priority);
-    }
-
-    sort(n);
-    findCompletionTime(n);
-    findTurnAroundTime(n);
-    findWaitingTime(n);
-
-    printf("PID\tAT\tBT\tCT\tTAT\tWT");
-    for (int i = 0; i < n; i++) {
-        printf("\n%d\t%d\t%d\t%d\t%d\t%d", processes[i].pid, processes[i].arrivalTime, processes[i].burstTime, processes[i].completionTime, processes[i].turn_aroundtime, processes[i].waitingTime);
-    }
-
-    findAvg(n);
     printf("\n");
-    return 0;
 }
