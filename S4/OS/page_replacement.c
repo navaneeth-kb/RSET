ccad1eb70e3bf00d041fb9310263d865106a1a5e
Navaneeth K.B <navaneethkb04@gmail.com>
2024-05-20 20:57:30 +0530

    Update page_replacement.c

--- S4/OS/page_replacement.c
+++ S4/OS/page_replacement.c
@@ -1,103 +1,118 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<stdlib.h>
 
-void simulateFIFO(int pages[], int n, int frames) {
-    int frame[frames], front = 0, pageFaults = 0;
-    for (int i = 0; i < frames; i++) 
-        frame[i] = -1;
-
-    printf("Reference String\tFrames\n");
-    for (int i = 0; i < n; i++) {
-        int found = 0;
-        for (int j = 0; j < frames; j++) {
-            if (frame[j] == pages[i]) {
-                found = 1;
+void simulateFIFO(int pages[],int n,int frames)
+{
+    int frame[frames],pageFault=0,found=0,front=0;
+    for(int i=0;i<frames;i++)
+    {
+        frame[i]=-1;
+    }
+    printf("Reference Strings\tFrames\n");
+    for(int i=0;i<n;i++)
+    {
+        found=0;
+        for(int j=0;j<frames;j++)
+        {
+            if(frame[j]==pages[i])
+            {
+                found=1;
                 break;
             }
         }
-        if (!found) {
-            frame[front] = pages[i];
-            front = (front + 1) % frames;
-            pageFaults++;
+        if(found==0)
+        {
+            frame[front]=pages[i];
+            front=(front+1)%frames;
+            pageFault++;
         }
-        printf("%d\t\t\t", pages[i]);
-        for (int j = 0; j < frames; j++) {
-            if (frame[j] != -1) 
-                printf("%d ", frame[j]);
-            else 
-                printf("  ");
+        printf("%d\t\t\t",pages[i]);
+        for(int j=0;j<frames;j++)
+        {
+            if(frame[j]==-1)
+            {
+                printf(" \t");
+            }
+            else
+            {
+                printf("%d\t",frame[j]);
+            }
         }
         printf("\n");
     }
-    printf("Total Page Faults: %d\n", pageFaults);
+    printf("Total Page Faults:%d\n",pageFault);
 }
-
-void simulateLRU(int pages[], int n, int frames) {
-    int frame[frames], counter[frames], time = 0, pageFaults = 0;
-    for (int i = 0; i < frames; i++) frame[i] = -1;
-    printf("Reference String\tFrames\n");
-    for (int i = 0; i < n; i++) {
-        int found = 0, min = 0;
-        for (int j = 0; j < frames; j++) {
-            if (frame[j] == pages[i]) {
-                found = 1;
-                counter[j] = time++;
+void simulateLRU(int pages[],int n,int frames)
+{
+    int frame[frames],counter[frames],time=0,pageFaults=0;
+    for(int i=0;i<frames;i++)
+    {
+        frame[i]=-1;
+        counter[i]=-1;
+    }
+    printf("Reference Strings\tFrames\n)");
+    for(int i=0;i<n;i++)
+    {
+        int found=0,min=0;
+        for(int j=0;j<frames;j++)
+        {
+            if(frame[j]==pages[i])
+            {
+                found=1;
+                counter[j]=time++;
                 break;
             }
         }
-        if (!found) {
-            for (int j = 0; j < frames; j++) {
-                if (frame[j] == -1) {
-                    min = j;
+        if (found==0)
+        {
+            for(int j=0;j<frames;j++)
+            {
+                if(frame[j]==-1)
+                {
+                    min=j;
                     break;
-                } else if (counter[j] < counter[min]) {
-                      min = j;
+                }
+                else if(counter[j]<counter[min])
+                {
+                    min=j;
                 }
             }
-            frame[min] = pages[i];
-            counter[min] = time++;
+            frame[min]=pages[i];
+            counter[min]=time++;
             pageFaults++;
         }
-        printf("%d\t\t\t", pages[i]);
-        for (int j = 0; j < frames; j++) {
-            if (frame[j] != -1) printf("%d ", frame[j]);
-            else printf("  ");
+        printf("%d\t\t\t",pages[i]);
+        for(int j=0;j<frames;j++)
+        {
+            if(frame[j]==-1)
+            {
+                printf(" ");
+            }
+            else
+            {
+                printf("%d\t",frame[j]);
+            }
         }
         printf("\n");
     }
-    printf("Total Page Faults: %d\n", pageFaults);
+    printf("Total Page Faults:%d\n",pageFaults);
 }
 
-int main() {
+void main()
+{
     int n, frame;
     int refString[100];
     printf("Enter length of reference string: ");
     scanf("%d", &n);
     printf("Enter reference string: ");
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         scanf("%d", &refString[i]);
     }
     printf("Enter number of frames: ");
     scanf("%d", &frame);
-
-    int choice;
-
-    printf("Choose Page Replacement Algorithm:\n");
-    printf("1. FIFO (First-In, First-Out)\n");
-    printf("2. LRU (Least Recently Used)\n");
-    do{
-    printf("Enter your choice (1 or 2): ");
-    scanf("%d", &choice);
-    switch (choice) {
-        case 1:
-            simulateFIFO(refString, n, frame);
-            break;
-        case 2:
-            simulateLRU(refString, n, frame);
-            break;
-        case 3:exit(0);
-        default:
-            printf("Invalid choice!\n");
-    }
-    }while(choice!=3);
-    return 0;
+    printf("FIFO\n");
+    simulateFIFO(refString, n, frame);
+    printf("LRU\n");
+    simulateLRU(refString, n, frame);
 }
