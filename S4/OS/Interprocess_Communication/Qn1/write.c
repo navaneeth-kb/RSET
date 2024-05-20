619a386f6e2e9b6a7100e7490b8c0cb15c1643b1
Navaneeth K.B <navaneethkb04@gmail.com>
2024-05-21 00:21:29 +0530

    Update write.c

--- S4/OS/Interprocess_Communication/Qn1/write.c
+++ S4/OS/Interprocess_Communication/Qn1/write.c
@@ -1,32 +1,29 @@
 #include <sys/shm.h>
 #include <stdio.h>
-int main()
+void main()
 {
     int i, n, r, fact=1, fact1=1, *shared_memory, buff[100], shmid;
     key_t key = 111;
     shmid = shmget(key, 1024, 0666 | IPC_CREAT);
     shared_memory = (int *)shmat(shmid, NULL, 0);
-    printf(&quot;Enter the integer n:&quot;);
-    scanf(&quot;%d&quot;, &amp;n);
+    printf("Enter the integer n:");
+    scanf("%d",&n);
     shared_memory[1]=n;
-    printf(&quot;The number in shared memory is %d\n&quot;,shared_memory[1]);
-    for(i=1;i&lt;=n;i++)
+    for(i=1;i<=n;i++)
     {
         fact=fact*i;
     }
     shared_memory[2] = fact;
-    printf(&quot;The factorial is:&quot;);
-    printf(&quot;%d\n&quot;, shared_memory[2]);
-    printf(&quot;Enter the integer r:&quot;);
+    printf("The factorial is:");
+    printf("%d\n", shared_memory[2]);
+    printf("Enter the integer r:");
     scanf(&quot;%d&quot;, &amp;r);
     shared_memory[3]=r;
-    printf(&quot;The number in shared memory is %d\n&quot;,shared_memory[3]);
     for(i=1;i&lt;=r;i++)
     {
         fact1=fact1*i;
     }
     shared_memory[4] = fact1;
-    printf(&quot;The factorial is:&quot;);
-    printf(&quot;%d\n&quot;, shared_memory[4]);
-    return 0;
+    printf("The factorial is:");
+    printf("%d\n", shared_memory[4]);
 }
