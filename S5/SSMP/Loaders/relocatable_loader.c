ef493e51f6ee213f350e41c5a0dcfed8c9797556
Navaneeth K.B <navaneethkb04@gmail.com>
2024-12-13 13:35:37 +0530

    Update relocatable_loader.c

--- S5/SSMP/Loaders/relocatable_loader.c
+++ S5/SSMP/Loaders/relocatable_loader.c
@@ -1,113 +1,85 @@
-#define _GNU_SOURCE
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-void convert(char h[12]);
-char bitmask[12];
-char bit[12] = {0};
+char bitmask[4];
+char binary_mask[13];
 
-void main() {
-    char add[6], length[10], input[10], binary[12], relocbit, ch, pn[5];
-    int start, inp, len, i, address, opcode, addr, actualadd, tlen;
-    FILE *fp1, *fp2;
-
-    printf("\n\n Enter the actual starting address: ");
-    scanf("%x", &start);
-
-    fp1 = fopen("objectcode_2.txt", "r");
-    fp2 = fopen("RLOUT.txt", "w");
-
-    if (fp1 == NULL || fp2 == NULL) {
-        printf("Error opening files.\n");
-        exit(1);
-    }
-
-    fscanf(fp1, "%s", input);
-    fprintf(fp2, "Address\tContent\n");
-
-    while (strcmp(input, "E") != 0) {
-        if (strcmp(input, "H") == 0) {
-            fscanf(fp1, "%s", pn);
-            fscanf(fp1, "%s", add);
-            fscanf(fp1, "%s", length);
-            fscanf(fp1, "%s", input);
-        }
-
-        if (strcmp(input, "T") == 0) {
-            fscanf(fp1, "%x", &address);
-            fscanf(fp1, "%x", &tlen);
-            fscanf(fp1, "%s", bitmask);
-
-            address += start;
-            convert(bitmask);
-
-            len = strlen(bit);
-            if (len >= 11) {
-                len = 10;
-            }
-
-            for (i = 0; i < len; i++) {
-                fscanf(fp1, "%x", &opcode);
-                fscanf(fp1, "%x", &addr);
-                relocbit = bit[i];
-
-                if (relocbit == '0') {
-                    actualadd = addr;
-                } else {
-                    actualadd = addr + start;
-                }
-
-                fprintf(fp2, "%x\t\t%x%x\n", address, opcode, actualadd);
-                address += 3;
-            }
-
-            fscanf(fp1, "%s", input);
+void convert_binary(char bitmask[4]) {
+    strcpy(binary_mask, "");
+    for (int i = 0; i < strlen(bitmask); i++) {
+        switch (bitmask[i]) {
+            case '0': strcat(binary_mask, "0000"); break;
+            case '1': strcat(binary_mask, "0001"); break;
+            case '2': strcat(binary_mask, "0010"); break;
+            case '3': strcat(binary_mask, "0011"); break;
+            case '4': strcat(binary_mask, "0100"); break;
+            case '5': strcat(binary_mask, "0101"); break;
+            case '6': strcat(binary_mask, "0110"); break;
+            case '7': strcat(binary_mask, "0111"); break;
+            case '8': strcat(binary_mask, "1000"); break;
+            case '9': strcat(binary_mask, "1001"); break;
+            case 'A': strcat(binary_mask, "1010"); break;
+            case 'B': strcat(binary_mask, "1011"); break;
+            case 'C': strcat(binary_mask, "1100"); break;
+            case 'D': strcat(binary_mask, "1101"); break;
+            case 'E': strcat(binary_mask, "1110"); break;
+            case 'F': strcat(binary_mask, "1111"); break;
         }
     }
-
-    fclose(fp1);
-    fclose(fp2);
-
-    printf("\n\n The contents of output file\n");
-    fp2 = fopen("RLOUT.txt", "r");
-    if (fp2 == NULL) {
-        printf("Error opening RLOUT.txt\n");
-        exit(1);
-    }
-
-    ch = fgetc(fp2);
-    while (ch != EOF) {
-        printf("%c", ch);
-        ch = fgetc(fp2);
-    }
-    
-    fclose(fp2);
 }
 
-void convert(char h[12]) {
-    int i, l;
-    strcpy(bit, "");
-    l = strlen(h);
+int main() {
+    int start_addr, address;
+    int opcode, operand;
+    char col_one[10];
+    char prog_name[10];
 
-    for (i = 0; i < l; i++) {
-        switch (h[i]) {
-            case '0': strcat(bit, "0"); break;
-            case '1': strcat(bit, "1"); break;
-            case '2': strcat(bit, "10"); break;
-            case '3': strcat(bit, "11"); break;
-            case '4': strcat(bit, "100"); break;
-            case '5': strcat(bit, "101"); break;
-            case '6': strcat(bit, "110"); break;
-            case '7': strcat(bit, "111"); break;
-            case '8': strcat(bit, "1000"); break;
-            case '9': strcat(bit, "1001"); break;
-            case 'A': strcat(bit, "1010"); break;
-            case 'B': strcat(bit, "1011"); break;
-            case 'C': strcat(bit, "1100"); break;
-            case 'D': strcat(bit, "1101"); break;
-            case 'E': strcat(bit, "1110"); break;
-            case 'F': strcat(bit, "1111"); break;
+    FILE *fp = fopen("input.txt", "r");
+    if (!fp) {
+        printf("Error opening file.\n");
+        return 1;
+    }
+
+    printf("Enter starting address: ");
+    scanf("%x", &start_addr);
+
+    printf("\nRelocated Address\tObject Code");
+
+    fscanf(fp, "%s", col_one);
+    while (strcmp(col_one, "E") != 0) {
+
+        if (strcmp(col_one, "H") == 0) {
+            fscanf(fp, "%s %*s %*s", prog_name);  // Skip record address and length
+            fscanf(fp, "%s", col_one);
+        }
+
+        if (strcmp(col_one, "T") == 0) {
+            fscanf(fp, "%x %*x %s", &address, bitmask);
+            address += start_addr;
+            convert_binary(bitmask);
+
+            int mask_index = 0;
+            while (fscanf(fp, "%02x%04x", &opcode, &operand) == 2) {
+                if (binary_mask[mask_index] == '1') {
+                    operand += start_addr; // Relocate operand if bit is 1
+                }
+
+                printf("\n%06x\t\t%02x%04x", address, opcode, operand);
+                address += 3;
+                mask_index++;
+
+
+                if (getc(fp) == '$') {
+                    break;
+                }
+            }
+
+            fscanf(fp, "%s", col_one); // Move
         }
     }
+
+    fclose(fp);
+    printf("\nEnd of program.\n");
+    return 0;
 }
