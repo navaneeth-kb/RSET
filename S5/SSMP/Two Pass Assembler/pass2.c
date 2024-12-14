43b4394fe245c6673325a33f25f8ec58e3c88f32
Navaneeth K.B <navaneethkb04@gmail.com>
2024-12-15 01:00:30 +0530

    Update pass2.c

--- S5/SSMP/Two Pass Assembler/pass2.c
+++ S5/SSMP/Two Pass Assembler/pass2.c
@@ -1,169 +1,146 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
-char *searchOptab(char *mnemonic)
+char code[20];
+char *searchoptab(char *opc)
 {
-    FILE *optab;
-    static char opcode[10];
-    char op[10], code[10];
-
-    optab = fopen("Optab.txt", "r");
-    if (optab == NULL)
+    FILE *optab_ptr=fopen("optab.txt","r");
+    if (optab_ptr == NULL)
     {
         printf("Error opening Optab.txt\n");
         exit(1);
     }
+    char opcode[20];
 
-    while (fscanf(optab, "%s %s", op, code) != EOF)
+    while((fscanf(optab_ptr,"%s %s",opcode,code))!=EOF)
     {
-        if (strcmp(op, mnemonic) == 0)
+        if ((strcmp(opcode,opc)==0))
         {
-            strcpy(opcode, code);
-            fclose(optab);
-            return opcode;
+            fclose(optab_ptr);
+            return code;
         }
     }
-
-    fclose(optab);
     return NULL;
+    fclose(optab_ptr);
 }
 
-int searchSymtab(char *label)
+int adr;
+int searchsymtab(char *op)
 {
-    FILE *symtab;
-    char sym[20];
-    int address;
-
-    symtab = fopen("SYMTAB.txt", "r");
-    if (symtab == NULL)
+    FILE *symtab_ptr=fopen("symtab.txt","r");
+    if (symtab_ptr == NULL)
     {
-        return -1;
-    }
-    while (fscanf(symtab, "%s %X", sym, &address) != EOF)
-    {
-        if (strcmp(sym, label) == 0)
-        {
-            fclose(symtab);
-            return address; // Label found
-        }
-    }
-
-    fclose(symtab);
-    return -1; // Label not found
-}
-
-int main()
-{
-    FILE *intermediate, *objectCode, *lengthFile;
-    char label[20], opcode[20], operand[20], objectCodeBuffer[100];
-    int locctr, startAddress, address, programLength, objectCodeLength;
-    char *op;
-    int textRecordLength = 0;
-    int maxTextRecordLength = 9;
-
-    intermediate = fopen("Intermediate.txt", "r");
-    objectCode = fopen("ObjectCode.txt", "w");
-    lengthFile = fopen("Length.txt", "r");
-
-    if (intermediate == NULL || objectCode == NULL || lengthFile == NULL)
-    {
-        printf("Error opening files\n");
+        printf("Error opening symtab.txt\n");
         exit(1);
     }
+    char opr[20];
 
-    fscanf(lengthFile, "Program length: %X", &programLength);
-    fclose(lengthFile);
-
-    fscanf(intermediate, "%X %s %s %s", &locctr, label, opcode, operand);
-    if (strcmp(opcode, "START") == 0)
+    while((fscanf(symtab_ptr,"%s %X",opr,&adr))!=EOF)
     {
-        startAddress = locctr;                                                       // Store the start address for the END record
-        fprintf(objectCode, "H^%s^%06X^%06X\n", label, startAddress, programLength); // Header record
+        if ((strcmp(opr,op)==0))
+        {
+            fclose(symtab_ptr);
+            return adr;
+        }
+    }
+    return -1;
+    fclose(symtab_ptr);
+}
+
+void main()
+{
+    FILE *intermediate_ptr=fopen("intermediate.txt","r");
+    FILE *length_ptr=fopen("length.txt","r");
+    FILE *objectcode_ptr=fopen("objectcode.txt","w");
+
+    int locctr,start_adr,length;
+    char label[20],opcode[20],operand[20];
+
+    fscanf(length_ptr,"Program length is %X",&length);
+    fscanf(intermediate_ptr,"%X %s %s %s",&locctr,label,opcode,operand);
+    if (strcmp(opcode,"START")==0)
+    {
+        start_adr=locctr;
+        fprintf(objectcode_ptr,"H^%s^%06X^%06X\n",label,locctr,length);
     }
 
-    objectCodeBuffer[0] = '\0';
-    textRecordLength = 0;
-    int textRecordStart = locctr;
+    char objectcodebuffer[100]="";
+    int textrecordstart=start_adr;
+    int textrecordlength=0;
 
-    while (fscanf(intermediate, "%X %s %s %s", &locctr, label, opcode, operand) != EOF)
+    while((fscanf(intermediate_ptr,"%X %s %s %s",&locctr,label,opcode,operand))!=EOF)
     {
-        // Handle the 'END' directive
-        if (strcmp(opcode, "END") == 0)
+        if(strcmp(opcode,"END")==0)
         {
-            if (textRecordLength > 0)
+            if(textrecordlength>0)
             {
-                fprintf(objectCode, "T^%06X^%02X^%s\n", textRecordStart, textRecordLength, objectCodeBuffer);
+                fprintf(objectcode_ptr,"T^%06X^%02X^%s\n",textrecordstart,textrecordlength,objectcodebuffer);
             }
-            fprintf(objectCode, "E^%06X\n", startAddress); // End record
-            break;
+            fprintf(objectcode_ptr,"E^%06X\n",start_adr);
         }
 
-        char currentObjectCode[20] = "";
+        char currentobjectcode[50]="";
+        int address;
 
-        op = searchOptab(opcode);
-        if (op != NULL)
+        char *op=searchoptab(opcode);
+        if(op!=NULL)
         {
-
-            address = searchSymtab(operand);
-            if (address != -1)
+            address=searchsymtab(operand);
+            if (address!=-1)
             {
-                // object code (opcode + address)
-                sprintf(currentObjectCode, "%s%04X", op, address);
+                sprintf(currentobjectcode,"%02s%04X",op,address);
             }
-            else if (strcmp(operand, "-") == 0)
+            else if((strcmp(operand,"-"))==0)
             {
-                // Instruction RSUB
-                sprintf(currentObjectCode, "%s0000", op);
+                sprintf(currentobjectcode,"%02s%0000",op);
             }
         }
-        else if (strcmp(opcode, "WORD") == 0)
+        else if((strcmp(opcode,"WORD"))==0)
         {
-            int value = atoi(operand);
-            sprintf(currentObjectCode, "%06X", value); // WORD is 3 bytes
+            int value=atoi(operand);
+            sprintf(currentobjectcode,"%06X",value);
         }
-        else if (strcmp(opcode, "BYTE") == 0)
+        else if((strcmp(opcode,"BYTE"))==0)
         {
-            if (operand[0] == 'C')
+            if (operand[0]=='C')
             {
-                // BYTE C'...'
-                for (int i = 2; i < strlen(operand) - 1; i++)
+                for (int i=2;i<strlen(operand)-3;i++)
                 {
-                    char hexValue[3];
-                    sprintf(hexValue, "%02X", (unsigned char)operand[i]);
-                    strcat(currentObjectCode, hexValue);
+                    char hexval[3];
+                    sprintf(hexval,"%02X",operand[i]);
+                    strcat(currentobjectcode,hexval);
                 }
             }
-            else if (operand[0] == 'X')
+            else if(operand[0]=='X')
             {
-                // BYTE X'...'
-                strncpy(currentObjectCode, operand + 2, strlen(operand) - 3); // Extract the hex value
-                currentObjectCode[strlen(operand) - 3] = '\0';
+                strncpy(currentobjectcode,operand+2,strlen(operand)-3);
+                currentobjectcode[strlen(operand)-3]='\0';
             }
         }
-        objectCodeLength = strlen(currentObjectCode) / 2;
-        int new;
-        if (textRecordLength + objectCodeLength > maxTextRecordLength)
+
+        int objectcodelength=(strlen(currentobjectcode))/2;
+
+        int num=0;
+        if(objectcodelength+textrecordlength > 9)
         {
-            fprintf(objectCode, "T^%06X^%02X^%s\n", textRecordStart, textRecordLength, objectCodeBuffer);
-            // Start a new text record
-            strcpy(objectCodeBuffer, ""); // Reset buffer
-            textRecordLength = 0;
-            textRecordStart = locctr;
-            new = 1;
+            fprintf(objectcode_ptr,"T^%06X^%02X^%s\n",textrecordstart,textrecordlength,objectcodebuffer);
+            strcpy(objectcodebuffer,"");
+            textrecordstart=locctr;
+            textrecordlength=0;
+            num=1;
         }
-        if (textRecordLength > 0 && new != 1)
+
+        if(textrecordlength>0 && num!=1)
         {
-            strcat(objectCodeBuffer, "^"); // Add separator before appending the next object code
+            strcat(objectcodebuffer,"^");
         }
-        strcat(objectCodeBuffer, currentObjectCode);
-        textRecordLength += objectCodeLength;
+        strcat(objectcodebuffer, currentobjectcode);
+        textrecordlength += objectcodelength;
     }
 
-    fclose(intermediate);
-    fclose(objectCode);
+    fclose(intermediate_ptr);
+    fclose(objectcode_ptr);
 
     printf("Pass 2 completed. Object program generated in ObjectCode.txt.\n");
-
-    return 0;
 }
