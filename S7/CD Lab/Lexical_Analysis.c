717f89cb3dac3077add307066fb2fd209126d966
Navaneeth K.B <navaneethkb04@gmail.com>
2025-11-11 11:12:32 +0530

    Update Lexical_Analysis.c

--- S7/CD Lab/Lexical_Analysis.c
+++ S7/CD Lab/Lexical_Analysis.c
@@ -1,108 +1,138 @@
-#include <stdio.h>
-#include <ctype.h>
+#include<stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-char *keywords[] = {"auto","break","case","char","const","continue","default","do","double","else","enum","extern","float","for","goto","if","int","long","register","return","short","signed","sizeof",
+char *keywords[]=  {"auto","break","case","char","const","continue","default","do","double","else","enum","extern","float","for","goto","if","int","long","register","return","short","signed","sizeof",
 "static","struct","switch","typedef","union","unsigned","void","volatile","while"};
 
-int isKeyword(char *str)
+int isKeyword(char *key)
 {
-    for (int i = 0; i < sizeof(keywords) / sizeof(keywords[0]); i++)
-        if (strcmp(str, keywords[i]) == 0)
+    for (int i=0;i<(sizeof(keywords)/sizeof(keywords[0]));i++)
+    {
+        if (strcmp(keywords[i],key)==0)
+        {
             return 1;
-	    return 0;
+        }
+    }
+    return 0;
 }
 
 int main()
 {
-    FILE *fp = fopen("input.c", "r");
-    char ch, buffer[100];
-    int i;
+    FILE *fp=fopen("test.txt","r");
+    char ch,buffer[100];
+
     if (!fp)
     {
-        printf("Cannot open file\n");
+        printf("Cant open file");
         return 0;
     }
 
-    while ((ch = fgetc(fp)) != EOF)
+    while ((ch=fgetc(fp))!=EOF)
     {
         if (isspace(ch))
-            continue;
-
-        switch (ch)
         {
-        case '=':
-        case '>':
-        case '<':
-        case '!':
-            buffer[0] = ch;
-            buffer[1] = fgetc(fp);
-            if (buffer[1] == '=')
-                buffer[2] = '\0';
-            else
-            {
-                ungetc(buffer[1], fp);
-                buffer[1] = '\0';
-            }
-            printf("Relational Operator: %s\n", buffer);
-            break;
+            continue;
+        }
 
-        case '+':
-        case '-':
-        case '*':
-        case '/':
-        case '%':
-            buffer[0] = ch;
-            buffer[1] = '\0';
-            if (ch == '/' && (buffer[1] = fgetc(fp)) == '/')
+        switch(ch)
+        {
+            case '=':
+            case '>':
+            case '<':
+            case '!':
             {
-                while ((ch = fgetc(fp)) != '\n' && ch != EOF)
-                    ;
-            }
-            else if (ch == '/' && buffer[1] == '*')
-            {
-                while ((ch = fgetc(fp)) != EOF)
-                    if (ch == '*' && fgetc(fp) == '/')
-                        break;
-            }
-            else
-            {
-                if (buffer[1])
-                    ungetc(buffer[1], fp);
-                printf("Arithmetic Operator: %c\n", ch);
-            }
-            break;
-
-        case '#':
-            while ((ch = fgetc(fp)) != '\n' && ch != EOF)
-                ;
-            break;
-
-        default:
-            if (isalpha(ch) || ch == '_')
-            {
-                i = 0;
-                do
-                    buffer[i++] = ch;
-                while (isalnum(ch = fgetc(fp)) || ch == '_');
-                buffer[i] = '\0';
-                ungetc(ch, fp);
-                if (isKeyword(buffer))
-                    printf("Keyword: %s\n", buffer);
+                buffer[0]=ch;
+                buffer[1]=fgetc(fp);
+                if (buffer[1]=='=')
+                {
+                    buffer[2]='\0';
+                }
                 else
-                    printf("Identifier: %s\n", buffer);
+                {
+                    ungetc(buffer[1],fp);
+                    buffer[1]='\0';
+                }
+                printf("Relational operator: %s\n",buffer);
+                break;
             }
-            else if (isdigit(ch))
+
+            case '+':
+            case '-':
+            case '*':
+            case '/':
+            case '%':
             {
-                i = 0;
-                do
-                    buffer[i++] = ch;
-                while (isdigit(ch = fgetc(fp)));
-                buffer[i] = '\0';
-                ungetc(ch, fp);
-                printf("Number: %s\n", buffer);
+                buffer[0]=ch;
+                buffer[1]=fgetc(fp);
+                buffer[2]='\0';
+                if (buffer[0]=='/' && buffer[1]=='/')
+                {
+                    while((ch=fgetc(fp))!='\n' && ch!=EOF)
+                        ;
+                }
+                else if (buffer[0]=='/' && buffer[1]=='*')
+                {
+                    while((ch=fgetc(fp))!=EOF)
+                    {
+                        if (ch=='*' && fgetc(fp)=='/')
+                        {
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    ungetc(buffer[1],fp);
+                    ungetc(buffer[2],fp);
+                    buffer[1]='\0';
+                    printf("Arithmetic operator: %c\n",ch);
+                }
+                break;
             }
-            break;
+
+            case '#':
+            {
+                while((ch=fgetc(fp))!='\n' && ch!=EOF)
+                    ;
+                break;
+            }
+
+            default:
+            {
+                if (isalpha(ch) || ch=='_')
+                {
+                    int i=0;
+                    do
+                    {
+                        buffer[i++]=ch;
+                    }
+                    while ((isalnum(ch=fgetc(fp))) || ch=='_');
+                    buffer[i]='\0';
+                    ungetc(ch,fp);
+                    if (isKeyword(buffer))
+                    {
+                        printf("Keyword: %s\n",buffer);
+                    }
+                    else
+                    {
+                        printf("Identifier: %s\n",buffer);
+                    }
+                }
+                if (isdigit(ch))
+                {
+                    int i=0;
+                    do
+                    {
+                        buffer[i++]=ch;
+                    }
+                    while (isdigit(ch=fgetc(fp)));
+                    buffer[i]='\0';
+                    ungetc(ch,fp);
+                    printf("Digit: %s\n",buffer);
+                }
+                break;
+            }
         }
     }
 
